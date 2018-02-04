3d3e6d770abed5b744ba8ddcb163326faa37daf9
Vasiliy <techyourchance@gmail.com>
2018-02-04 21:06:58 +0200

    BAD! Convention over configuration: injecting public non-static non-final fields

--- app/src/main/java/com/techyourchance/journeytodependencyinjection/common/dependencyinjection/Injector.java
+++ app/src/main/java/com/techyourchance/journeytodependencyinjection/common/dependencyinjection/Injector.java
@@ -1,7 +1,12 @@
 package com.techyourchance.journeytodependencyinjection.common.dependencyinjection;
 
-import com.techyourchance.journeytodependencyinjection.screens.questiondetails.QuestionDetailsActivity;
-import com.techyourchance.journeytodependencyinjection.screens.questionslist.QuestionsListActivity;
+import com.techyourchance.journeytodependencyinjection.questions.FetchQuestionDetailsUseCase;
+import com.techyourchance.journeytodependencyinjection.questions.FetchQuestionsListUseCase;
+import com.techyourchance.journeytodependencyinjection.screens.common.dialogs.DialogsManager;
+import com.techyourchance.journeytodependencyinjection.screens.common.mvcviews.ViewMvcFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class Injector {
 
@@ -12,26 +17,47 @@ public class Injector {
     }
 
     public void inject(Object client) {
-        if (client instanceof QuestionsListActivity) {
-            injectQuestionsListActivity((QuestionsListActivity) client);
+        Class clazz = client.getClass();
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            if (isPublicNotStaticNotFinal(field)) {
+                injectField(client, field);
+            }
         }
-        else if (client instanceof QuestionDetailsActivity) {
-            injectQuestionDetailsActivity((QuestionDetailsActivity) client);
+    }
+
+    private boolean isPublicNotStaticNotFinal(Field field) {
+        int modifiers = field.getModifiers();
+        return Modifier.isPublic(modifiers) && !Modifier.isStatic(modifiers) && !Modifier.isFinal(modifiers);
+    }
+
+    private void injectField(Object client, Field field) {
+        try {
+            field.set(client, getServiceForClass(field.getType()));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object getServiceForClass(Class<?> type) {
+        if (type.equals(DialogsManager.class)) {
+            return mPresentationCompositionRoot.getDialogsManager();
+        }
+        else if (type.equals(ViewMvcFactory.class)) {
+            return mPresentationCompositionRoot.getViewMvcFactory();
+        }
+        else if (type.equals(FetchQuestionsListUseCase.class)) {
+            return mPresentationCompositionRoot.getFetchQuestionsListUseCase();
+        }
+        else if (type.equals(FetchQuestionDetailsUseCase.class)) {
+            return mPresentationCompositionRoot.getFetchQuestionDetailsUseCase();
         }
         else {
-            throw new RuntimeException("invalid client: " + client);
+            throw new RuntimeException("unsupported service type class: " + type);
         }
     }
 
-    private void injectQuestionsListActivity(QuestionsListActivity client) {
-        client.mViewMvcFactory = mPresentationCompositionRoot.getViewMvcFactory();
-        client.mDialogsManager = mPresentationCompositionRoot.getDialogsManager();
-        client.mFetchQuestionsListUseCase = mPresentationCompositionRoot.getFetchQuestionsListUseCase();
-    }
 
-    private void injectQuestionDetailsActivity(QuestionDetailsActivity client) {
-        client.mViewMvcFactory = mPresentationCompositionRoot.getViewMvcFactory();
-        client.mDialogsManager = mPresentationCompositionRoot.getDialogsManager();
-        client.mFetchQuestionDetailsUseCase = mPresentationCompositionRoot.getFetchQuestionDetailsUseCase();
-    }
 }
