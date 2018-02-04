1322dbe6797aebbea9108141068c63d042e412d4
Vasiliy <techyourchance@gmail.com>
2018-02-04 15:09:33 +0200

    Extracting UI logic into MVC views

--- app/src/main/java/com/techyourchance/journeytodependencyinjection/screens/questionslist/QuestionsListActivity.java
+++ app/src/main/java/com/techyourchance/journeytodependencyinjection/screens/questionslist/QuestionsListActivity.java
@@ -3,23 +3,14 @@
  import android.os.Bundle;
  import android.support.v4.app.FragmentManager;
  import android.support.v7.app.AppCompatActivity;
- import android.support.v7.widget.LinearLayoutManager;
- import android.support.v7.widget.RecyclerView;
  import android.view.LayoutInflater;
- import android.view.View;
- import android.view.ViewGroup;
- import android.widget.TextView;
 
  import com.techyourchance.journeytodependencyinjection.Constants;
  import com.techyourchance.journeytodependencyinjection.networking.QuestionsListResponseSchema;
- import com.techyourchance.journeytodependencyinjection.R;
- import com.techyourchance.journeytodependencyinjection.screens.common.ServerErrorDialogFragment;
  import com.techyourchance.journeytodependencyinjection.networking.StackoverflowApi;
  import com.techyourchance.journeytodependencyinjection.questions.Question;
+ import com.techyourchance.journeytodependencyinjection.screens.common.ServerErrorDialogFragment;
  import com.techyourchance.journeytodependencyinjection.screens.questiondetails.QuestionDetailsActivity;
-
- import java.util.ArrayList;
- import java.util.List;
 
  import retrofit2.Call;
  import retrofit2.Callback;
@@ -28,32 +19,21 @@
  import retrofit2.converter.gson.GsonConverterFactory;
 
  public class QuestionsListActivity extends AppCompatActivity implements
-         Callback<QuestionsListResponseSchema> {
-
-     private RecyclerView mRecyclerView;
-     private QuestionsAdapter mQuestionsAdapter;
+         Callback<QuestionsListResponseSchema>,QuestionsListViewMvc.Listener {
 
      private StackoverflowApi mStackoverflowApi;
 
      private Call<QuestionsListResponseSchema> mCall;
 
+     private QuestionsListViewMvc mViewMvc;
+
      @Override
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
-         setContentView(R.layout.layout_questions_list);
 
-         // init recycler view
-         mRecyclerView = findViewById(R.id.recycler);
-         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+         mViewMvc = new QuestionsListViewMvcImpl(LayoutInflater.from(this), null);
 
-         mQuestionsAdapter = new QuestionsAdapter(new OnQuestionClickListener() {
-             @Override
-             public void onQuestionClicked(Question question) {
-                 QuestionDetailsActivity.start(QuestionsListActivity.this, question.getId());
-             }
-         });
-
-         mRecyclerView.setAdapter(mQuestionsAdapter);
+         setContentView(mViewMvc.getRootView());
 
          // init retrofit
          Retrofit retrofit = new Retrofit.Builder()
@@ -67,6 +47,7 @@
      @Override
      protected void onStart() {
          super.onStart();
+         mViewMvc.registerListener(this);
          mCall = mStackoverflowApi.lastActiveQuestions(20);
          mCall.enqueue(this);
      }
@@ -74,6 +55,7 @@
      @Override
      protected void onStop() {
          super.onStop();
+         mViewMvc.unregisterListener(this);
          if (mCall != null) {
              mCall.cancel();
          }
@@ -83,7 +65,7 @@
      public void onResponse(Call<QuestionsListResponseSchema> call, Response<QuestionsListResponseSchema> response) {
          QuestionsListResponseSchema responseSchema;
          if (response.isSuccessful() && (responseSchema = response.body()) != null) {
-             mQuestionsAdapter.bindData(responseSchema.getQuestions());
+             mViewMvc.bindQuestions(responseSchema.getQuestions());
          } else {
              onFailure(call, null);
          }
@@ -97,62 +79,8 @@
                  .commitAllowingStateLoss();
      }
 
-     // --------------------------------------------------------------------------------------------
-     // RecyclerView adapter
-     // --------------------------------------------------------------------------------------------
-
-
-     public interface OnQuestionClickListener {
-         void onQuestionClicked(Question question);
-     }
-
-     public static class QuestionsAdapter extends RecyclerView.Adapter<QuestionsAdapter.QuestionViewHolder> {
-
-         private final OnQuestionClickListener mOnQuestionClickListener;
-
-         private List<Question> mQuestionsList = new ArrayList<>(0);
-
-         public class QuestionViewHolder extends RecyclerView.ViewHolder {
-             public TextView mTitle;
-
-             public QuestionViewHolder(View view) {
-                 super(view);
-                 mTitle = view.findViewById(R.id.txt_title);
-             }
-         }
-
-         public QuestionsAdapter(OnQuestionClickListener onQuestionClickListener) {
-             mOnQuestionClickListener = onQuestionClickListener;
-         }
-
-         public void bindData(List<Question> questions) {
-             mQuestionsList = new ArrayList<>(questions);
-             notifyDataSetChanged();
-         }
-
-         @Override
-         public QuestionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-             View itemView = LayoutInflater.from(parent.getContext())
-                     .inflate(R.layout.layout_question_list_item, parent, false);
-
-             return new QuestionViewHolder(itemView);
-         }
-
-         @Override
-         public void onBindViewHolder(QuestionViewHolder holder, final int position) {
-             holder.mTitle.setText(mQuestionsList.get(position).getTitle());
-
-             holder.itemView.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View view) {
-                     mOnQuestionClickListener.onQuestionClicked(mQuestionsList.get(position));
-                 }
-             });
-         }
-
-         @Override
-         public int getItemCount() {
-             return mQuestionsList.size();
-         }
+     @Override
+     public void onQuestionClicked(Question question) {
+         QuestionDetailsActivity.start(QuestionsListActivity.this, question.getId());
      }
  }
